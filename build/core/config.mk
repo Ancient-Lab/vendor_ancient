8977000b5b0b04e669a93c3feacaad70ea8a5f31
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-03 12:25:55 +0100

    Never allow neverallows
    Change-Id: Ibf07e9e319d676666e3e7389720c24917e38a9ac

--- build/core/config.mk
+++ build/core/config.mk
@@ -16,11 +16,6 @@
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/ion/build/core/system_rro.mk
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for MTK targets
 include $(TOPDIR)vendor/ion/build/core/mtk_target.mk
 
