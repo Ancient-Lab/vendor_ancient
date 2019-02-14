89b81f85034d47284130935e3fa36f32d5eabbd6
AnkitGourav <Ankitgourav035@gmail.com>
2019-02-15 00:03:00 +0530

    Add device repo

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,11 +43,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/pixel.xml"
+custom_local_manifest = ".repo/local_manifests/device.xml"
 custom_default_revision = "pie"
 custom_dependencies = "aosp.dependencies"
-org_manifest = "pixel-devices"  # leave empty if org is provided in manifest
-org_display = "PixelExperience-Devices"  # needed for displaying
+org_manifest = "device"  # leave empty if org is provided in manifest
+org_display = "ion-OS"  # needed for displaying
 
 github_auth = None
 
