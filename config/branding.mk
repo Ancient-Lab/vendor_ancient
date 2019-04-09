047e32bc0f8b117670ec123d5e58f37c6c222cbc
AnkitGourav <Ankitgourav035@gmail.com>
2019-04-09 21:34:44 +0530

    ion-1.0.e

--- config/branding.mk
+++ config/branding.mk
@@ -2,7 +2,7 @@
 ION_BUILD_TYPE ?= UNOFFICIAL
 ION_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 ION_PLATFORM_VERSION := 9.0
-IONIZER := 1.0.d
+IONIZER := 1.0.e
 
 TARGET_PRODUCT_SHORT := $(subst ion_,,$(ION_BUILD))
 
