0da5c60e0c9a164bdf6a86746975f6a0af8a152f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-04 00:25:31 -0300

    vendor: Whitelist LiveDisplay permissions
    Change-Id: Iefa5365b6ec013f164a338038b0d13c45b369a13

--- config/common.mk
+++ config/common.mk
@@ -62,7 +62,8 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/ion/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
     vendor/ion/config/permissions/privapp-permissions-aosp.xml:system/etc/permissions/privapp-permissions-aosp.xml \
-    vendor/ion/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml
+    vendor/ion/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml \
+    vendor/ion/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml
 
 # init.d support
 PRODUCT_COPY_FILES += \
