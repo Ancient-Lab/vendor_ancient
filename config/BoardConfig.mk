deda4a21ae93b3a86221acf598d7263fbf3eb35a
Dyneteve <sound0020@gmail.com>
2019-09-27 15:11:44 +0000

    Revert "vendor: Add special conditions for devices with prebuilt display hal"
    This reverts commit 8a9cf11622e671e4115f831f1e30353164fad23a.

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -2,12 +2,6 @@ include vendor/ion/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/ion/config/BoardConfigQcom.mk
-else
-
-ifneq ($(FORCE_QCOM_DISPLAY_HAL_VARIANT),)
-include vendor/ion/config/BoardConfigQcomDisplayOverride.mk
-endif
-
 endif
 
 include vendor/ion/config/BoardConfigSoong.mk
