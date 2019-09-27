0b5851fc40c95bdf71bb10b2028ec8cb563c042e
AnkitGourav <Ankitgourav035@gmail.com>
2019-09-27 15:49:54 +0530

    Add ion release_type

--- config/branding.mk
+++ config/branding.mk
@@ -1,12 +1,13 @@
 # Set all versions
 ION_BUILD_TYPE ?= UNOFFICIAL
 ION_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+ION_RELEASE_TYPE ?= Preview
 ION_PLATFORM_VERSION := 10.0
 IONIZER := 2.0.a
 
 TARGET_PRODUCT_SHORT := $(subst ion_,,$(ION_BUILD))
 
-ION_VERSION := ion-$(IONIZER)-$(ION_BUILD)-$(ION_BUILD_DATE)-$(ION_BUILD_TYPE)
+ION_VERSION := ion-$(IONIZER)-$(ION_BUILD)-$(ION_BUILD_DATE)-$(ION_BUILD_TYPE)-$(ION_RELEASE_TYPE)
 ION_FINGERPRINT := ion/$(ION_PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(ION_BUILD_DATE)
 
 ION_PROPERTIES := \
@@ -14,4 +15,5 @@ ION_PROPERTIES := \
     ro.ion.build_date=$(ION_BUILD_DATE) \
     ro.ion.build_type=$(ION_BUILD_TYPE) \
     ro.ion.fingerprint=$(ION_FINGERPRINT) \
+    ro.ion.release_type=$(ION_RELEASE_TYPE) \
     ro.ionizer=$(IONIZER)
