LineageOS/android_vendor_lineage

build: dt_image: use prebuilt lz4 binary for compressing
* Also remove the lz4c fallback. We're providing an lz4
  binary that works, so it shouldn't ever have to fall back
  to lz4c.

Change-Id: I5b1346c55dc97daec13e087c501840628a7265e8

build/tasks/dt_image.mk
@@ -42,7 +42,7 @@ endif
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
 	$(build-dtimage-target)
 ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-	lz4 -9 < $@ > $(LZ4_DT_IMAGE) || lz4c -c1 -y $@ $(LZ4_DT_IMAGE)
+	prebuilts/tools-lineage/${HOST_OS}-x86/bin/lz4 -9 < $@ > $(LZ4_DT_IMAGE)
 	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
 endif
 	@echo "Made DT image: $@"

Ancient-Lab/vendor_ancient

build/tasks/dt_image.mk
@@ -42,7 +42,7 @@ endif
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
 	$(build-dtimage-target)
 ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-	lz4 -9 < $@ > $(LZ4_DT_IMAGE) || lz4c -c1 -y $@ $(LZ4_DT_IMAGE)
+	prebuilts/tools-ion/${HOST_OS}-x86/bin/lz4 -9 < $@ > $(LZ4_DT_IMAGE)
 	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
 endif
 	@echo "Made DT image: $@"