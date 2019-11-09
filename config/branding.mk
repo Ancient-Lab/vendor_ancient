f5eef67d82151464175b0b8d6653e40655652c08
AnkitGourav <Ankitgourav035@gmail.com>
2019-11-10 00:41:56 +0530

    ion-2.1.a & keep ion version a step ahead on preview builds

--- config/branding.mk
+++ config/branding.mk
@@ -3,7 +3,12 @@ ION_BUILD_TYPE ?= UNOFFICIAL
 ION_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 ION_RELEASE_TYPE ?= Preview
 ION_PLATFORM_VERSION := 10.0
-IONIZER := 2.0.c
+
+ifeq ($(ION_RELEASE_TYPE), Release)
+    IONIZER := 2.1.a
+else
+    IONIZER := 2.1.b
+endif
 
 TARGET_PRODUCT_SHORT := $(subst ion_,,$(ION_BUILD))
 
