b9c03eb786782c1b97c4dcc3950a3751364a9fcc
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-02 19:19:58 +0530

    vendor: whitelist priv-app permissions
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -61,6 +61,7 @@ endif
 # Some permissions
 PRODUCT_COPY_FILES += \
     vendor/ancient/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
+    vendor/ancient/config/permissions/privapp-permissions-ancient.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-ancient.xml \
     vendor/ancient/config/permissions/privapp-permissions-fm.xml:system/etc/permissions/privapp-permissions-fm.xml \
     vendor/ancient/config/permissions/privapp-permissions-snap.xml:system/etc/permissions/privapp-permissions-snap.xml \
     vendor/ancient/config/permissions/privapp-permissions-camera2.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-camera2.xml
