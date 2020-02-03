e6ea413f4739628df5eb4c2ccd309cc84e749c72
Dyneteve <dyneteve@pixelexperience.org>
2020-02-03 16:19:13 +0000

    vendor: config: Drop ro.ancient.fingerprint
    Change-Id: Iac6165e9e70ad8d00e0c459f7f49ae8716731918

--- config/branding.mk
+++ config/branding.mk
@@ -37,13 +37,11 @@ endif
 TARGET_PRODUCT_SHORT := $(subst ancient_,,$(ANCIENT_BUILD))
 
 ANCIENT_VERSION := AncientOS-$(ANCIENT_BASE_EDITION)-$(BUILD_VERSION)-$(ANCIENT_BUILD)-$(ANCIENT_BUILD_TYPE)-$(ANCIENT_BUILD_DATE)
-ANCIENT_FINGERPRINT := Ancient/$(ANCIENT_PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(ANCIENT_BUILD_DATE)
 
 ANCIENT_PROPERTIES := \
     ro.ancient.custom.version=$(ANCIENT_VERSION_CUSTOM) \
     ro.ancient.version=$(ANCIENT_VERSION) \
     ro.ancient.build_date=$(ANCIENT_BUILD_DATE) \
     ro.ancient.build_type=$(ANCIENT_BUILD_TYPE) \
-    ro.ancient.fingerprint=$(ANCIENT_FINGERPRINT) \
     ro.ancient.release_type=$(ANCIENT_BUILD_TYPE) \
     ro.ancient=$(ANCIENT)
