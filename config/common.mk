dba9bfb138337fc97dd19c1645b2917f3c0bf6ca
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-08 23:23:14 -0300

    vendor: Add hide notch support [2/3]
    Change-Id: I1474bf4deb83bcbf714905e820485e0559a1b776

--- config/common.mk
+++ config/common.mk
@@ -112,6 +112,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/ancient/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/ancient/overlay/common
 
+# Cutout control overlay
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # PixelSetupWizard overlay
 PRODUCT_PACKAGES += \
     PixelSetupWizardOverlay \
