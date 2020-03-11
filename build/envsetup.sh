dcff307f31cd75e7e7bbc1d20c5f8350e2687a8e
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-11 12:24:26 +0100

    vendor: envsetup: s/wait-for-online/wait-for-device-recovery/g
    Change-Id: I3fd52aa99d281837fdbdba3b1d268572061a900d

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -97,7 +97,7 @@ function eat()
             return 1
         fi
         echo "Waiting for device..."
-        adb wait-for-online
+        adb wait-for-device-recovery
         echo "Found device"
         if (adb shell getprop ro.ancient.device | grep -q "$ANCIENT_BUILD"); then
             echo "Rebooting to sideload for install"
@@ -317,9 +317,9 @@ function installboot()
             return 1
         fi
     fi
-    adb wait-for-online
+    adb wait-for-device-recovery
     adb root
-    adb wait-for-online
+    adb wait-for-device-recovery
     if (adb shell getprop ro.ancient.device | grep -q "$ANCIENT_BUILD");
     then
         adb push $OUT/boot.img /cache/
@@ -355,9 +355,9 @@ function installrecovery()
             return 1
         fi
     fi
-    adb wait-for-online
+    adb wait-for-device-recovery
     adb root
-    adb wait-for-online
+    adb wait-for-device-recovery
     if (adb shell getprop ro.ancient.device | grep -q "$ANCIENT_BUILD");
     then
         adb push $OUT/recovery.img /cache/
@@ -756,7 +756,6 @@ function dopush()
         adb connect "$TCPIPPORT"
     fi
     adb wait-for-device &> /dev/null
-    sleep 0.3
     adb remount &> /dev/null
 
     mkdir -p $OUT
