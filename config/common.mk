7350dd9e6c3383f16ae05c0c3dd7aaba0ab68f28
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-07-21 09:46:48 +0700

    Revert "Revert "vendor: packages: include plugins packages""
    This reverts commit 171275089e8465f6d9f525e34ea4638639b96cc0.

--- config/common.mk
+++ config/common.mk
@@ -160,6 +160,9 @@ include vendor/ancient/prebuilt/common/fonts/fonts.mk
 # Packages
 include vendor/ancient/config/packages.mk
 
+# Plugins
+include packages/apps/Plugins/plugins.mk
+
 $(call inherit-product-if-exists, external/motorola/faceunlock/config.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
